5191b6a8bc791a568aba18bff62787dad3d8a763
Devin Black <Devinjamesblack@gmail.com>
2020-04-11 19:52:30 -0600

    a second example of interfaces

--- main/main.go
+++ main/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+// Concret is person
 type person struct {
 	first string
 }
 
 func (p person) speak() {
 	fmt.Println("from a person - this is my name", p.first)
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("I'm a secret agent -- this is my name", sa.first)
 }
 
 // any Type that has the methods specified by an interface
@@ -19,14 +29,23 @@ type human interface {
 }
 
 func main() {
+	// Abstract type is p1
 	p1 := person{
 		first: "Devin",
+	}
+	sal := secretAgent{
+		person: person{
+			first: "DevJBlack",
+		},
+		ltk: true,
 	}
 
 	fmt.Printf("%T\n", p1)
 	// in go a VALUE can be of more than one TYPE
 	// in this example, p1 is both TYPE person and human
-	var x human
+	var x, y human
 	x = p1
-	fmt.Printf("%T\n", x)
+	y = sal
+	x.speak()
+	y.speak()
 }
