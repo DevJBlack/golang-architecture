8edff8383c4a5d9894dff8640d4c3ff436a2c9e7
Devin Black <Devinjamesblack@gmail.com>
2020-04-17 06:03:16 -0600

    context with time out and done channels

--- context02/main.go
+++ context02/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/DevJBlack/golang-architecture/tree/master/context02/session"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = session.SetUserId(ctx, 1)
+	ctx = session.SetAdminAccess(ctx, true)
+
+	uID := session.GetUserId(ctx)
+	isAdmin := session.GetAdmin(ctx)
+	fmt.Printf("Use %d is an admin %t", uID, isAdmin)
 }
