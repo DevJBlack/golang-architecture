4ecbdc9a5f7fc966edbb93b1b94384d75d9afd32
Devin Black <Devinjamesblack@gmail.com>
2020-04-20 06:50:55 -0600

    equality on structs

--- stringMethod/main.go
+++ stringMethod/main.go
@@ -14,5 +14,17 @@ func main() {
 	p1 := person{
 		first: "Devin",
 	}
-	fmt.Println(p1)
+
+	p2 := person{
+		first: "James",
+	}
+
+	m := map[person]string{}
+	m[p1] = "My father"
+	m[p2] = "Father"
+
+	fmt.Println(m[p1])
+	fmt.Println(m[p2])
+	fmt.Println(p1 == p2)
+	fmt.Println(p1 == p1)
 }
